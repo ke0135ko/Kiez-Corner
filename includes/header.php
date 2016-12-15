639ce528652eb35110923a111b47e2a6eaaff9f8
ke0135ko <kevin.kosinski@fh-erfurt.de>
2016-12-15 08:25:16 +0100

    ok

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,16 @@
 <header>
+    <a class="logo" href="index.php">
+        <img src="img/logo.png" alt="Logo">
+    </a>
     <!--Hier spaeter Ausgabe von evtl. zwoschengespeicherten Mitteilungen einfuegen-->
-        <ul class="topnav" id="myTopnav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="index.php?page=products">Inserate</a></li>
-            <li><a href="index.php?page=about">Suche</a></li>
-            <li><a href="index.php?page=contact">Impressum</a></li>
-            <li class="icon">
-            <a href="javascript:void(0);" onclick="myFunction.js">&#9776;</a>
-            </li>
+    <nav>
+        <ul>
+            <li class="<?php if ($page == 'start') echo "active"; ?>"><a href="index.php">Home</a></li>
+            <li class="<?php if ($page == 'products') echo "active"; ?>"><a href="index.php?page=products">Produkte</a></li>
+            <li class="<?php if ($page == 'about') echo "active"; ?>"><a href="index.php?page=about">About</a></li>
+            <li class="<?php if ($page == 'contact') echo "active"; ?>"><a href="index.php?page=contact">Kontakt</a></li>
             <!--Hier spaeter Login-Prüfung und -Button einfuegen-->
         </ul>
+    </nav> 
+    <div class="clear"></div>
 </header>
